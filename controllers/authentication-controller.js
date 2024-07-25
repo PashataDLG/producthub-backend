ad68b7bf54b3ee9ec88810d510d8a8a4c55fef56
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-25 22:42:20 +0300

    login functionallity created

--- controllers/authentication-controller.js
+++ controllers/authentication-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user-model.js');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     const { username, password } = req.body;
@@ -16,4 +17,21 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: 'Invalid credentials! ' });
+        }
+
+        const token = jwt.sign({ id: user._id }, 'jwt-secret', { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { register, login };
