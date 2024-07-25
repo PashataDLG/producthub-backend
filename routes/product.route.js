dcca69b71a0da8a2e3b6f69ff2395904a134a994
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-25 23:36:58 +0300

    authentication route guard created and added to routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product.controller.js');
 const validateProduct = require('../middlewares/validateProduct.js');
+const authenticate = require('../middlewares/auth.js');
 
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProduct);
-router.post('/', validateProduct, productController.createProduct);
-router.put('/:id', validateProduct, productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.post('/', authenticate, validateProduct, productController.createProduct);
+router.put('/:id', authenticate, validateProduct, productController.updateProduct);
+router.delete('/:id', authenticate, productController.deleteProduct);
 
 module.exports = router;
