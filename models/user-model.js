7111ef27d04559b55f7e5589f9ccb790bd08b43f
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-25 23:55:04 +0300

    fixed typo in the user model

--- models/user-model.js
+++ models/user-model.js
@@ -13,14 +13,17 @@ const userSchema = mongoose.Schema({
     }
 });
 
-userSchema.pre('Save', async function(next) {
+userSchema.pre('save', async function(next) {
     if(!this.isModified('password')) {
         return next();
-    };
+    }
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (error) {
+        next(error)
+    }
 });
 
 userSchema.methods.comparePassword = function(enteredPassword) {
