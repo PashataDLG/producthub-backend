fd054cf4c31907e88e9625796e1282e13d300aa5
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-25 22:11:48 +0300

    Added logic to check password before being saved.

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     username: {
@@ -10,6 +11,16 @@ const userSchema = mongoose.Schema({
         type: string,
         required: true,
     }
+});
+
+userSchema.pre('Save', async function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
