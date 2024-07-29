b1d784225b5c7f115e7413a67bc5301a4d2444f0
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-29 20:35:52 +0000

    added end point for logout, and route guard

--- controllers/authentication-controller.js
+++ controllers/authentication-controller.js
@@ -42,7 +42,7 @@ const logout = async (req, res) => {
     const authHeader = req.header('Authorization');
 
     if (!authHeader) {
-        return res.status(401).json({ message: 'Access deniend. No token provided!' });
+        return res.status(401).json({ message: 'Access denied. No token provided!' });
     };
 
     const token = authHeader.replace('Bearer ', '');
--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authentication-controller.js');
+const authenticate = require('../middlewares/auth.js');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout', authenticate, authController.logout);
 
 module.exports = router;
