7cec437055e41bb2e8cb460aa0d8f41dd27ac560
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-29 21:45:19 +0000

    Added login and register validation to the respective endpoints

--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authentication-controller.js');
 const authenticate = require('../middlewares/auth.js');
+const validateLogin = require('../middlewares/validateLogin.js');
+const validateRegistration = require('../middlewares/validateRegistration.js');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validateRegistration, authController.register);
+router.post('/login', validateLogin, authController.login);
 router.post('/logout', authenticate, authController.logout);
 
 module.exports = router;
