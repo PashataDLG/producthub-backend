2d00c0dfc0b5cacb81b1308d0fe28eae9853a4a5
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-30 16:53:09 +0000

    Added the rate limiter to the route endpoint

--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authentication-controller.js');
+
 const authenticate = require('../middlewares/auth.js');
 const validateLogin = require('../middlewares/validateLogin.js');
 const validateRegistration = require('../middlewares/validateRegistration.js');
+const loginLimiter = require('../middlewares/rateLimiter.js');
 
 router.post('/register', validateRegistration, authController.register);
-router.post('/login', validateLogin, authController.login);
+router.post('/login', loginLimiter, validateLogin, authController.login);
 router.post('/logout', authenticate, authController.logout);
 
 module.exports = router;
