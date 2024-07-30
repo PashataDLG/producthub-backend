39f097b9cfb7f632e7c83733e1cad7401cebf598
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-30 13:11:31 +0300

    added timestamps for user model

--- models/product.model.js
+++ models/product.model.js
@@ -6,7 +6,7 @@ const productSchema = mongoose.Schema(
             type: String,
             required: [true, 'Product name is required']
         },
-        
+
         quantity: {
             type: Number,
             required: true,
