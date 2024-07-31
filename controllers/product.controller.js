4954bc04c63afcd59c4ae3d76b41014fd86e8bcf
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-31 15:18:39 +0000

    refactured product controller to use product service

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,8 +1,8 @@
-const Product = require('../models/product.model.js');
+const productService = require('../services/product.service.js');
 
 const getProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
+        const products = await productService.getAllProducts();
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -12,7 +12,8 @@ const getProducts = async (req, res) => {
 const getProduct = async (req, res) => {
     try {
         const { id } = req.params;
-        const product = await Product.findById(id);
+        const product = await productService.getProductById(id);
+
         res.status(200).json(product);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -21,7 +22,7 @@ const getProduct = async (req, res) => {
 
 const createProduct = async (req, res) => {
     try {
-        const newProduct = await Product.create(req.body);
+        const newProduct = await productService.createProduct(req.body);
         res.status(200).json(newProduct);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -31,13 +32,13 @@ const createProduct = async (req, res) => {
 const updateProduct = async (req, res) => {
     try {
         const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
+        const product = await productService.updateProduct(id, req.body);
 
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
 
-        const updatedProduct = await Product.findById(id);
+        const updatedProduct = await productService.getProductById(id);
 
         res.status(200).json(updatedProduct);
     } catch (error) {
@@ -49,7 +50,7 @@ const deleteProduct = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const product = await Product.findByIdAndDelete(id);
+        const product = await productService.deleteProduct(id);
 
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
