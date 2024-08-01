f8e37ac0a69c3c0c39e7776410b252167526bf2e
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-01 15:24:32 +0000

    Changed the user model file name

--- tests/authentication-controller.test.js
+++ tests/authentication-controller.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app.js');
 const mongoose = require('mongoose');
-const User = require('../models/User-Model.js');
+const User = require('../models/User.js');
 
 beforeAll(async () => {
     const dbURI = process.env.TEST_MONGODB_URI;
