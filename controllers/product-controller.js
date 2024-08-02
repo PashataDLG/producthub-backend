1c421cca0dc8844641bc77b7cf5ce6c375e4125d
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-02 13:12:48 +0300

    changed the message we receive when a product is deleted

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -56,7 +56,7 @@ const deleteProduct = async (req, res) => {
             return res.status(404).json({ message: 'Product not found' });
         }
 
-        res.status(200).json({ product: "Product deleted successfully" });
+        res.status(200).json({ message: "Product deleted successfully" });
     } catch (error) {
         s
         res.status(500).json({ message: error.message });
