999d383195202b2b6fa05f84c76e3d72c5560ef9
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-03 13:13:30 +0300

    removed login limiter due to unexpected behaviour

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -5,10 +5,9 @@ const authController = require('../controllers/authentication-controller.js');
 const authenticate = require('../middlewares/auth.js');
 const validateLogin = require('../middlewares/validateLogin.js');
 const validateRegistration = require('../middlewares/validateRegistration.js');
-const loginLimiter = require('../middlewares/rateLimiter.js');
 
 router.post('/register', validateRegistration, authController.register);
-router.post('/login', loginLimiter, validateLogin, authController.login);
+router.post('/login', validateLogin, authController.login);
 router.post('/logout', authenticate, authController.logout);
 
 module.exports = router;
