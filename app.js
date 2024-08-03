61d03f6f7516489a42e9de351cd3ce8275739779
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-03 19:15:51 +0300

    Removed comments we do not need

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to database!");
 
-        // Стартирай сървъра само ако не сме в тестова среда
         if (process.env.NODE_ENV !== 'test') {
             app.listen(port, () => {
                 console.log(`Server is running on port ${port}`);
